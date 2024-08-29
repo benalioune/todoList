9c796e3c574d74a4386096b3d0ffaf0893a65ae5
papa-alioune.diallo <papa-alioune.diallo@estiam.com>
2024-08-29 14:50:50 +0200

    changing local configs

--- database/firebase.py
+++ database/firebase.py
@@ -4,7 +4,7 @@ import pyrebase
 import json
 import os
 
-from configs.firebase_config import firebaseConfig
+from configs.firebase_config import FIREBASE_CONFIG
 
 from dotenv import load_dotenv
 
@@ -21,13 +21,13 @@ load_dotenv()
 # import des crédentiels
 #initialise our app import credentials if not done
 if not firebase_admin._apps: 
-    cred = credentials.Certificate("configs/firebase_service_account_key.json")
+    cred = credentials.Certificate("configs/firebaseServiceAccountKey.json")
     firebase_admin.initialize_app(cred)
 
 
 
 # getting access to firebase  by using rebase / create a new firebase instance  
-firebase = pyrebase.initialize_app(firebaseConfig)
+firebase = pyrebase.initialize_app(FIREBASE_CONFIG)
 # access to our database instance by pyrebase 
 db = firebase.database()
 authTodo = firebase.auth()
